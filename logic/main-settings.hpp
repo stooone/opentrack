e219f7fe746d8451f7a90b1188bcbf274c374046
JUHASZ Peter Karoly <stone@midway.hu>
2026-05-15 21:59:20 +0200

    0.06 degree per second not to feel nausea

--- logic/main-settings.hpp
+++ logic/main-settings.hpp
@@ -77,7 +77,7 @@ struct OTR_LOGIC_EXPORT main_settings final
 
     value<bool> center_at_startup { b, "center-at-startup", true };
     value<bool> auto_center { b, "auto-center", false };
-    value<double> auto_center_speed { b, "auto-center-speed", 0.33 };
+    value<double> auto_center_speed { b, "auto-center-speed", 0.06 };
     value<double> auto_center_deadzone { b, "auto-center-deadzone", 1.0 };
     value<centering_state> centering_mode { b, "centering-mode", center_roll_compensated };
     value<int> neck_z { b, "neck-depth", 0 };
